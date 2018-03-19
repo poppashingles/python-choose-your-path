da35fad09bf92ff4927d81ba80ad1f2b1a97d35b
Rob <robdwilliams85@gmail.com>
2018-03-19 07:05:29 +0000

    First bit of layout of the story

--- cypa.py
+++ cypa.py
@@ -1,1 +1,12 @@
-print("Hello")
+print("What your name is?")
+
+name = raw_input("")
+
+print("Hi Michelle! Yumana play some game?")
+
+ans = raw_input("")
+
+if ans == "yes":
+    print("oo de lally")
+else:
+    print("okbye i hope snakes eat you")
