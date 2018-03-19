2514b7b9575edc43c6397bd3f8c9ac899b3097df
Rob <robdwilliams85@gmail.com>
2018-03-19 16:11:31 +0000

    Three questions and answers

--- cypa.py
+++ cypa.py
@@ -1,12 +1,20 @@
-print("What your name is?")
+name = raw_input("What is your name? ").strip().title()
 
-name = raw_input("")
+ans = raw_input("Hi Michelle! Yumana play some game? ")
 
-print("Hi Michelle! Yumana play some game?")
-
-ans = raw_input("")
+def question2():
+    ans2 = input("How many pigs do you take with you to change a lightbulb? ")
+    if ans2 < 10:
+        print("Correct! More than 10 pigs is too many pigs")
+    else:
+        print("Goodness me, you're positively packing that place with pigs. You're absolutely mental. You die of pig-cramping, obviously. What were you thinking?")
 
 if ans == "yes":
     print("oo de lally")
+    question2()
 else:
     print("okbye i hope snakes eat you")
+
+
+
+print("Thanks for having a play!")
