279f1d051863b3ca0ac5f13abd15b4e4e3d10cd7
Rob <robdwilliams85@gmail.com>
2018-03-22 05:15:31 +0000

    Game complete, expansion as a work in progress

--- cypa.py
+++ cypa.py
@@ -3,9 +3,11 @@
 # 4 | 5 | 6
 # 1 | 2 | 3
 
+
 # Global variables for win conditions
 bottle = False
 shotgun = False
+
 
 # To check if grid squares have been visited already in order to change messages
 # if they are visited a second time
@@ -19,13 +21,11 @@ grid7Visited = False
 grid8Visited = False
 grid9Visited = False
 
-#  Intro
-name = raw_input("What is your name? \n").strip().title()
-ans = raw_input("Hi Michelle! Yumana play some game? \n")
 
 #  Grid content
 def grid9():
     grid9Visited = True
+
     if bottle == True and shotgun == False:
         ans9 = raw_input("""You finally, after literally minutes of this adventure, meet the wizard that John McCaine mentioned back in grid 6.
         It looks more like a tired old man with a scraggly beard than a wizard though.
@@ -35,14 +35,30 @@ def grid9():
             You had zero evidence of that.\n """)
         elif ans9.upper() == "NO":
             print("Well...yeah, obviously. Why would you murder an old man based on the word of a clearly insane John McCaine? You win!")
+
     elif bottle == False and shotgun == True:
-        ans9 = raw_input("""You finally, after literally minutes of this adventure, meet the wizard that John McCaine mentioned back in grid 6.
-        It looks more like a tired old man with a scraggly beard than a wizard though.
+        ans9 = raw_input("""You come across an old man (not like that) standing on a small island with a little coconut tree.
         Well, you have that shotgun from the HMS Sugababes Past. Shoot him with it?\n """)
         if ans9.upper() == "YES":
             print("""You've murdered a lost old man. Nice work....just....based on what?! Why would you do that?!\n """)
         elif ans9.upper() == "NO":
             print("Well...yeah, obviously. Why would you murder an old man? You win!")
+
+    elif bottle == True and shotgun == True:
+        ans9 = raw_input("""You finally, after literally minutes of this adventure, meet the wizard that John McCaine mentioned back in grid 6.
+        It looks more like a tired old man with a scraggly beard than a wizard though.
+        Well, you have that broken bottle. Stab him with it?\n """)
+        if ans9.upper() == "YES":
+            print("""You've murdered a lost old man. Nice work....just....based on what?! Why would you do that?!\n """)
+        elif ans9.upper() == "NO":
+            ans9b = raw_input("""Good choice, it'd be much more fun to shoot him with that shotgun you picked up from the
+            Ship of the Damned Sugababes. Is that your thinking?\n """)
+            if ans9.upper() == "YES":
+                print("""You've murdered a lost old man. Nice work. Why would he be a wizard?!
+                You had zero evidence of that.\n """)
+            elif ans9.upper() == "NO":
+                print("Well...yeah, obviously. Why would you murder an old man based on the word of a clearly insane John McCaine? You win!\n ")
+
     else:
         ans9 = raw_input("""You meet an old dude, he's just kind of standing there...
         Just leave? I guess? South or West are the only options here...\n """)
@@ -51,11 +67,13 @@ def grid9():
         elif ans9.upper() == "SOUTH":
             grid6()
 
+
 def grid8():
     grid8Visited = True
     ans8 = raw_input("""You draw closer to the boat, it's super spooky and gets super spookier the closer you get.
     Might be worth sending Anneka on ahead, she's been singing showtunes and "cleaning" herself like a cat all the way here.
     What do you reckon?\n """)
+
     if ans8.upper() == "YES":
         print("""You pretend you need to tie your shoelace, which is tricky in the sea and you almost drown in commitment to the lie.
         She swims on ahead while you carry on not-quite-drowning. As you break the surface,
@@ -69,8 +87,10 @@ def grid8():
         You'd spent years wondering what happened to all of the former sugababes, so that's one mystery solved at least.
         Anyway, there's a gun within your reach, so if you want to have a go at retiring them permanently, go for it. I'm not sure
         why you would, but it's been an unusual Saint Lidwina's Day overall so sod it. Fight or run?\n """)
+
         global shotgun
         shotgun = True
+
         if ans8b.upper() == "FIGHT":
             print("""They've spent their years on the sea living well, pirating from the wealthy. Your skills are no match for theirs
             and though you wing Keisha you go down quickly. Too late you learn the lesson not to fight a pirate
@@ -82,12 +102,16 @@ def grid8():
 
 def grid7():
     grid7Visited = True
+
     print("You fell off the edge, that was silly. You big silly goose. Also on the way down you're eaten by geese.\n")
+
 
 def grid6():
     grid6Visited = True
+
     ans6 = raw_input("""Bless his little cotton socks, it's a big pointy hat that says 'Oh I'm just the smelliest'.
     Help him take it off or just laugh at his plight? Nobody will judge you, it's fine. Help or laugh?\n """)
+
     if ans6.upper() == "HELP":
         print("""You climb up onto the island and take the hat off of his head.
         He looks you in the eye and says 'thank you for your help young'un' and presents you an item wrapped in cloth.
@@ -104,6 +128,7 @@ def grid6():
 
 def grid5():
     grid5Visited = True
+
     ans5 = raw_input("""Hooray! A lack of bears. But you are in the sea (remember? it said earlier). That's not where bears live.
     You curse your damn fool self and take a look about.
     You take a look west. It's that ocelot again, singing away. He might have sweets in his pockets...
@@ -113,6 +138,7 @@ def grid5():
     It looks deserted, but in stories deserted things usually aren't because otherwise they wouldn't be there.
     Narratively speaking it's just pointless. Honestly though it could go either way, I've not written it yet.
     Which way draws your fancy? West to Sweets, East to McCaine or North to a possibly not deserted small boat?\n """)
+
     if ans5.upper() == "WEST":
         grid4()
     elif ans5.upper() == "EAST":
@@ -120,36 +146,51 @@ def grid5():
     elif ans5.upper() == "NORTH":
         grid8()
 
+
 def grid4():
     grid4Visited = True
+
     print("""It sings ever so sweetly that you fall asleep and drown.
     That's possible apparently but you might be the first person it's happened to.
     Congratulations on a...I guess, interesting death. See you in Valhalla\n""")
 
+
 def grid3():
     grid3Visited = True
+
     print("Argh! Bears! You're dead! (from bears)\n")
+
 
 def grid2():
     grid2Visited = True
+
     ans2 = raw_input("""Anneka sees a fellow adventurer and decides to come along with you
     after a final scream at the seagulls about life insurance premiums.
     You've gained a new companion! She is the worst. North? Maybe bears. I'd try East?\n """)
+
     if ans2.upper() == "NORTH":
         grid5()
     elif ans2.upper() == "EAST":
         grid3()
 
+
 def grid1():
     grid1Visited = True
+
     ans1 = raw_input("""You wake up in the sea, what the fuck happened there?!
     Just north of you there's a small golden ocelot hovering above the sea and
     singing a gentle lullaby about gentlemen and their ways.
     To the east, Anekka Rice begs seagulls for challenges. Which way will you go?\n """)
+
     if ans1.upper() == "NORTH":
         grid4()
     elif ans1.upper() == "EAST":
         grid2()
+
+
+#  Intro
+name = raw_input("What is your name? \n").strip().title()
+ans = raw_input("Hi Michelle! Yumana play some game? \n")
 
 if ans.upper() == "YES":
     grid1()
